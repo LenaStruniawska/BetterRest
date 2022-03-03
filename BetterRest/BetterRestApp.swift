4239dcf2ffcd2da9dae20ecc6ff6977f6e9925fe
Magdalena Struniawska <mag.sok@onet.pl>
2022-03-03 11:52:31 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Slawek on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
